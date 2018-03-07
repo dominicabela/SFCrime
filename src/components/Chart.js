4ddabb2ca96bbaa0c245069a343a9dc222cd2e09
Dominic Abela <d.j.m.abela@gmail.com>
2018-03-06 21:04:15 -0500

    Change Title label

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -23,7 +23,7 @@ class Chart extends Component {
           options={{
             title: {
               display: this.props.displayTitle,
-              text: 'Largest Cities In Massachusetts',
+              text: 'San Francisco Crime Data',
               fontSize: 25
             },
             legend: {
